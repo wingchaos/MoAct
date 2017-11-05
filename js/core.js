7f60abbe4503a33a07eac481366fa7632f113d51
WingChaos <540459901@qq.com>
2017-11-05 20:48:58 +0800

    update core.js

--- js/core.js
+++ js/core.js
@@ -18,6 +18,7 @@ var QueryString = function () {
 }();
 
 var host_port = QueryString.HOST_PORT;
+if (host_port){
 while (host_port.endsWith('/')) {
     host_port = host_port.substring(0, host_port.length - 1)
 }
@@ -38,6 +39,8 @@ if (wsUri.indexOf("ws://") == 0 || wsUri.indexOf("wss://") == 0) {
         wsUri = "ws://" + wsUri.replace(/@HOST_PORT@/im, host_port)
     }
 }
+else
+	wsUri="ws://act.s1.natapp.cc";
 
 class ActWebsocketInterface {
     constructor(uri, path = "MiniParse") {
